6e6f06d46a66127fccc4dd58b73afe627f4a8ca1
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-03-14 13:37:50 +0300

    refactor keyboards to multilanguage

--- keyboards.py
+++ keyboards.py
@@ -4,19 +4,63 @@ from aiogram.types import ReplyKeyboardRemove, \
 from typing import List
 
 start = '✍️ Начать'
+start = {
+    'ru' : '✍️ Начать',
+    'en' : '✍️ Start'
+}
+
 finish = '💤 Закончить'
+finish = {
+    'ru' : '💤 Закончить',
+    'en' : '💤 Finish'
+}
+
 categories = '📌Категории'
+categories = {
+    'ru' : '📌Категории',
+    'en' : '📌Categories'
+}
+
 analysis = 'Аналитика'
+analysis = {
+    'ru' : 'Аналитика',
+    'en' : 'Analytics'
+}
+
 today_stat = '📈Статистика сегодня'
+today_stat = {
+    'ru' : '📈Статистика сегодня',
+    'en' : '📈Today stat'
+}
+
 help = '❓Помощь'
+help = {
+    'ru' : '❓Помощь',
+    'en' : '❓Help'
+}
+
 menu_kb = ReplyKeyboardMarkup(keyboard = [[start, finish],
                                           [categories, analysis],
                                           [today_stat, help]],
                               resize_keyboard=True,
                               one_time_keyboard=True)
 add = '➕Добавить'
+add = {
+    'ru' : '➕Добавить',
+    'en' : '➕Add'
+}
+
 remove = '♻️Удалить'
+remove = {
+    'ru' : '♻️Удалить',
+    'en' : '♻️Remove'
+}
+
 back = '🔙Назад'
+back = {
+    'ru' : '🔙Назад',
+    'en' : '🔙Back'
+}
 cats_kb = ReplyKeyboardMarkup(keyboard = [[add, remove],
                                           [back]],
                               resize_keyboard=True,
@@ -26,16 +70,24 @@ back_kb = ReplyKeyboardMarkup(keyboard = [[back]],
                               resize_keyboard=True,
                               one_time_keyboard=True)
 
-def compose_categories_kb(cats: List[str]) -> ReplyKeyboardMarkup:
+def compose_categories_kb(cats: List[str], lang: str = 'en') -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=3)
-    
     for cat in cats:
         kb.insert(KeyboardButton(cat))
-    kb.row(back)
+    kb.row(back[lang])
     return kb
 
 yes = 'Да'
+yes = {
+    'ru' : 'Да',
+    'en' : 'Yes'
+}
+
 no = 'Нет'
+no = {
+    'ru' : 'Нет',
+    'en' : 'No'
+}
 confirm_kb = ReplyKeyboardMarkup(keyboard = [[yes, no]],
                                 resize_keyboard=True,
                                 one_time_keyboard=True)
